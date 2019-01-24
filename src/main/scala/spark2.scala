47c7f84d91e5f8427117e727aa71048892b8b1ca
William Hatzar <williamhatzar15@hotmail.co.uk>
2019-01-24 16:00:39 +0000

    Apache Spark Practice

--- src/main/scala/spark2.scala
+++ src/main/scala/spark2.scala
@@ -24,18 +24,46 @@ object spark2 {
     val rdd4 = rdd3.map(x=>Row(x._1,x._2,x._3,x._4))
 
     val schema = StructType(
-      StructField("user id", IntegerType, true) ::
-        StructField("item id", IntegerType, true) ::
+      StructField("user_id", IntegerType, true) ::
+        StructField("movie_id", IntegerType, true) ::
         StructField("rating", IntegerType, true) ::
         StructField("timestamp", IntegerType, true) ::
         Nil
     )
 
     val DF10 = sqlContext.createDataFrame(rdd4,schema)
-    val DF11 = DF10.filter(DF10("rating")===5).groupBy("item id").count()
+    val DF11 = DF10.filter(DF10("rating")===5).groupBy("movie_id").count()
     val DF12 = DF11.orderBy(DF11("count").desc).show
+    val DF13 = DF10.groupBy("movie_id").agg(mean("Rating")as "Average_Rating")
+    val DF14 = DF13.orderBy(DF13("Average_Rating").desc).show
 
 
+    val rdd13 = sc.textFile("C:\\Users\\Admin\\Documents\\movies.txt")
+    val rdd14 = rdd13.map(x=>{
+      val record = x.split("\\|")
+      (record(0).toInt,record(1))
+    })
+
+    val rdd15 = rdd14.map(x=>Row(x._1,x._2))
+
+    val schema1 = StructType(
+      StructField("movie_id", IntegerType, true) ::
+      StructField("Movie_Name", StringType, true) ::
+      Nil
+    )
+
+    val DF20 = sqlContext.createDataFrame(rdd15,schema1)
+    val DF21 = DF20.filter(DF20("movie_id")===50).show
+
+    DF10.registerTempTable("Test_Table")
+    DF20.registerTempTable("m")
+    println("Showing results using SQL")
+    val results = sqlContext.sql("SELECT user_id, movie_id  FROM Test_Table WHERE rating = 5 AND movie_id >500").show()
+
+    def double(num1 :Int): Int = num1 * num1
+    val udfDoubler = udf(double(_),LongType)
+    sqlContext.udf.register("sqlDoubler",double(_))
+    sqlContext.sql("SELECT FROM Test_Table WHERE double(a")
 
 
   }
